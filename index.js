f36d1a492cbf30cc7a979e93104f87aa76219517
Mia Bueno <mia.bueno101@gmail.com>
2021-01-28 10:36:00 +1100

    change both img elements

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 
 // random number gen from 1 to 6
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomImg1 = "images/dice" + randomNumber1 + ".png"
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
+// concat random num into corresponding file name
+var randomImg1 = "images/dice" + randomNumber1 + ".png"
+var randomImg2 = "images/dice" + randomNumber2 + ".png"
+
+// change to random images
 document.querySelector(".img1").setAttribute("src", randomImg1);
+document.querySelector(".img2").setAttribute("src", randomImg2);
