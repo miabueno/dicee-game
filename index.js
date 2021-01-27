396b3c5665f1874bcc8c5ec7fa1e7798e71893af
Mia Bueno <mia.bueno101@gmail.com>
2021-01-28 10:34:01 +1100

    change player 1 image to random die

--- index.js
+++ index.js
@@ -3,3 +3,6 @@
 
 // random number gen from 1 to 6
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImg1 = "images/dice" + randomNumber1 + ".png"
+
+document.querySelector(".img1").setAttribute("src", randomImg1);
